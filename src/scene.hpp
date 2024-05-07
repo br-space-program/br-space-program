48783cad74878664d93915aa3f78ffc82f6b3ffd
Emre UCAR <emre.ucar@polytechnique.edu>
2024-05-07 07:59:34 +0200

    set up formatter

--- src/scene.hpp
+++ src/scene.hpp
@@ -3,7 +3,8 @@
 #include "cgp/cgp.hpp"
 #include "environment.hpp"
 
-// This definitions allow to use the structures: mesh, mesh_drawable, etc. without mentionning explicitly cgp::
+// This definitions allow to use the structures: mesh, mesh_drawable, etc.
+// without mentionning explicitly cgp::
 using cgp::mesh;
 using cgp::mesh_drawable;
 using cgp::numarray;
@@ -11,63 +12,64 @@ using cgp::timer_basic;
 using cgp::vec3;
 
 // Variables associated to the GUI (buttons, etc)
-struct gui_parameters
-{
-	bool display_frame = false;
-	bool display_wireframe = false;
+struct gui_parameters {
+  bool display_frame = false;
+  bool display_wireframe = false;
 
-	// vec3 light_color[] = [ {1, 1, 1}, {0.4, 0.6, 0.8}, {1, 1, 0.2} ];
-	// vec3 light_position[] = [ {-2, 2, 0.8}, {1, -3, 0.2}, {-2, 1, 1} ];
-	vec3 light_color = {1, 1, 1};
-	vec3 light_position = {-2, -2, 0.8};
+  // vec3 light_color[] = [ {1, 1, 1}, {0.4, 0.6, 0.8}, {1, 1, 0.2} ];
+  // vec3 light_position[] = [ {-2, 2, 0.8}, {1, -3, 0.2}, {-2, 1, 1} ];
+  vec3 light_color = {1, 1, 1};
+  vec3 light_position = {-2, -2, 0.8};
 
-	float ambiant = 0.3f;
-	float diffus = 0.8f;
-	float speculaire = 0.5f;
-	float exp_spec = 100.0f;
+  float ambiant = 0.3f;
+  float diffus = 0.8f;
+  float speculaire = 0.5f;
+  float exp_spec = 100.0f;
 };
 
 // The structure of the custom scene
-struct scene_structure : cgp::scene_inputs_generic
-{
+struct scene_structure : cgp::scene_inputs_generic {
+  // ****************************** //
+  // Elements and shapes of the scene
+  // ****************************** //
+  camera_controller_orbit_euler camera_control;
+  camera_projection_perspective camera_projection;
+  window_structure window;
 
-	// ****************************** //
-	// Elements and shapes of the scene
-	// ****************************** //
-	camera_controller_orbit_euler camera_control;
-	camera_projection_perspective camera_projection;
-	window_structure window;
+  mesh_drawable global_frame;         // The standard global frame
+  environment_structure environment;  // Standard environment controler
+  input_devices
+      inputs;  // Storage for inputs status (mouse, keyboard, window dimension)
+  gui_parameters gui;  // Standard GUI element storage
 
-	mesh_drawable global_frame;		   // The standard global frame
-	environment_structure environment; // Standard environment controler
-	input_devices inputs;			   // Storage for inputs status (mouse, keyboard, window dimension)
-	gui_parameters gui;				   // Standard GUI element storage
+  // ****************************** //
+  // Elements and shapes of the scene
+  // ****************************** //
 
-	// ****************************** //
-	// Elements and shapes of the scene
-	// ****************************** //
+  timer_basic timer;
+  mesh_drawable ground;
+  mesh_drawable cube;
 
-	timer_basic timer;
-	mesh_drawable ground;
-	mesh_drawable cube;
+  mesh_drawable sphere;
+  mesh_drawable camel;
 
-	mesh_drawable sphere;
-	mesh_drawable camel;
+  mesh_drawable sphere_light;
 
-	mesh_drawable sphere_light;
+  // ****************************** //
+  // Functions
+  // ****************************** //
 
-	// ****************************** //
-	// Functions
-	// ****************************** //
+  void initialize();  // Standard initialization to be called before the
+                      // animation loop
+  void
+  display_frame();  // The frame display to be called within the animation loop
+  void display_gui();  // The display of the GUI, also called within the
+                       // animation loop
 
-	void initialize();	  // Standard initialization to be called before the animation loop
-	void display_frame(); // The frame display to be called within the animation loop
-	void display_gui();	  // The display of the GUI, also called within the animation loop
+  void mouse_move_event();
+  void mouse_click_event();
+  void keyboard_event();
+  void idle_frame();
 
-	void mouse_move_event();
-	void mouse_click_event();
-	void keyboard_event();
-	void idle_frame();
-
-	void display_info();
+  void display_info();
 };
